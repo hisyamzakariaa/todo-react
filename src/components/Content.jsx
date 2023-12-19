63e88e2bf0dd7c51ac6ee3446f057b3dacaae3ae
hisyamzakariaa <135838950+hisyamzakariaa@users.noreply.github.com>
2023-12-19 21:25:56 +0800

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,16 +4,16 @@ const todosArr = JSON.parse(localStorage.getItem('todos'))
 
 export default function Content(){
 
-    function formatDate(date){
-        const monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    // function formatDate(date){
+    //     const monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-        let dateArr = date.split('-')
-        let day = dateArr[2]
-        let month = monthArr[dateArr[1] - 1]
-        let year = dateArr[0]
+    //     let dateArr = date.split('-')
+    //     let day = dateArr[2]
+    //     let month = monthArr[dateArr[1] - 1]
+    //     let year = dateArr[0]
         
-        return day + ' ' + month + ' ' + year
-    }
+    //     return day + ' ' + month + ' ' + year
+    // }
 
     function handleDelete(todo){
         todosArr.forEach((element,index) => {
@@ -31,7 +31,8 @@ export default function Content(){
             <div key={todo.title} className={classes.container}>
                 <h2>{todo.title}</h2>
                 <p className={classes.desc}>Description: {todo.description}</p>
-                <p>Due Date: {formatDate(todo.date)}</p>
+                {/* <p>Due Date: {formatDate(todo.date)}</p> */}
+                <p>Due Date: {todo.date}</p>
                 <button onClick={() => handleDelete(todo)}>Delete</button>
             </div>
         ))
